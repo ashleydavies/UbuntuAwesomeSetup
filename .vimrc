aa6efde5907506ef46d37bab457202cd659062a0
Ashley Davies-Lyons <ashley@daviesl.uk>
2015-12-30 14:17:27 +0000

    Added vimrc

---
+++ .vimrc
@@ -0,0 +1,25 @@
+set colorcolumn=80
+set ruler
+set backspace=eol,start,indent
+set ignorecase
+set smartcase
+set hlsearch
+set number
+set incsearch
+set showmatch
+set mat=2
+set noerrorbells
+set novisualbell
+set t_vb=
+set tm=500
+
+syntax enable
+set encoding=utf8
+set expandtab
+set smarttab
+set shiftwidth=4
+set tabstop=4
+set ai
+set si
+set wrap
+
--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-sudo apt-get install vim zsh git git-core
-mv .Xresources ~/.Xresources
+sudo apt-get install vim zsh git git-core ghc
+cp .vimrc ~/.vimrc
+cp .Xresources ~/.Xresources
 xrdb -merge ~/.Xresources
 curl http://www.ihdwallpaperx.com/wp-content/uploads/2015/03/SPACE-FULL-HD-WALLPAPER-25.jpg -o Pictures/space.jpg   
-mv theme.lua /usr/share/awesome/themes/default/theme.lua
+cp theme.lua /usr/share/awesome/themes/default/theme.lua
 git config --global user.name "Ashley Davies-Lyons"
 git config --global user.email "ashley@daviesl.uk"
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -11,6 +12,10 @@ cd
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 mv dircolors.ansi-dark .dircolors
 eval `dircolors ~/.dircolors`
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
 git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 ./set_dark.sh
